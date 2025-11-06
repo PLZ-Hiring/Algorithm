78f6954d6f7b04c0525cbaa40122904b37c085bb
gyeongbibin <151108886+gyeongbibin@users.noreply.github.com>
2025-11-06 15:55:08 +0900

    BaekJoon 2309 해답

---
+++ Week1/BOJ_2309.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").trim().split("\n").filter(Boolean);
+
+const heights = input.map(Number);
+const total = heights.reduce((a, b) => a + b, 0);
+
+for (let i = 0; i < 9; i++) {
+  for (let j = i + 1; j < 9; j++) {
+    if (total - (heights[i] + heights[j]) === 100) {
+
+      const result = heights
+        .filter((_, idx) => idx !== i && idx !== j)
+        .sort((a, b) => a - b);
+
+      console.log(result.join("\n"));
+      process.exit(0);  // 종료 
+    }
+  }
+}
