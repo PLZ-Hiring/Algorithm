d84df8f375a7e2be96641d58c985b4a66c8643fb
jyeon03 <bjlyeon2003@naver.com>
2026-01-08 21:20:28 +0900

    Baekjoon - 3986(좋은 단어)

---
+++ BAEKJIYEON/week4/3986.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  int N;
+  scanf("%d", &N);
+
+  for (int i = 0; i < N; i++) {
+    char s[101];
+    scanf("%s", &s);
+
+    char stack[101];
+
+    int top = 0;
+
+    
+  }
+}
